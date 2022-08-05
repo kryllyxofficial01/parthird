d5d8c31f04289ad7daa81d70c40e2444f377b0a5
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-08-05 18:24:17 +0000

    kick, ban, and unban

--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,47 @@
 import os
+from dotenv import load_dotenv
+import discord
 from discord.ext import commands
 
+# Creates the bot.
 client = commands.Bot(command_prefix="//")
+
+# Gets the bot's token.
+load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-print(TOKEN)
-
+# Notifier that the bot is online.
 @client.event
 async def on_ready():
 	print(f"Logged on as {client.user}")
 
+# Kicks the given user.
+@client.command()
+async def kick(ctx, user: discord.Member, *, reason):
+	await user.kick(reason=reason)
+	await ctx.send(f"User \"{user}\" has been kicked.\nReason: {reason}")
+	return
+
+# Bans the given user.
+@client.command()
+async def ban(ctx, user: discord.Member, *, reason):
+	await user.ban(reason=reason)
+	await ctx.send(f"User \"{user}\" has been banned.\nReason: {reason}")
+	return
+
+# Unbans the given user.
+@client.command()
+async def unban(ctx, *, user):
+	banned = await ctx.guild.bans()
+	username, tag = user.split("#")
+
+	for ban in banned:
+		usr = ban.user
+		if (usr.name, usr.discriminator) == (username, tag):
+			await ctx.guild.unban(usr)
+			await ctx.send(f"User \"{usr}\" has been unbanned.")
+	
+	return
+
+# Starts the bot.
 client.run(TOKEN)
