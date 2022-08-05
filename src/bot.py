2aa71c8ff07879a29e489c4a31f9ab0e38000c75
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-08-05 17:44:59 +0000

    setup

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,13 @@
+import os
+from discord.ext import commands
 
+client = commands.Bot(command_prefix="//")
+TOKEN = os.getenv("TOKEN")
+
+print(TOKEN)
+
+@client.event
+async def on_ready():
+	print(f"Logged on as {client.user}")
+
+client.run(TOKEN)
