e913e7d0625a3ed2f3556afb0867e3f4535b8342
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-08-07 00:48:26 +0000

    help command

--- src/bot.py
+++ src/bot.py
@@ -23,15 +23,73 @@ async def on_command_error(ctx, error):
         await ctx.send("You don't permission to use that command.")
         return
 
+# Help command
 @client.command()
 async def help(ctx, command=None):
-    embed = discord.Embed(
-		title="Parthird Help",
-		description="test",
-		colour=discord.Colour.dark_gray()
-	)
-        
-    await ctx.send(embed=embed)
+	if command == None:
+		embed = discord.Embed(
+			title = "__**Parthird Help**__",
+			description = "*List of all the commands Parthird has to offer.*\n---------------------------------------------------------------------------------------------------",
+			colour = discord.Colour.from_rgb(0,150,90)
+		)
+	
+		embed.add_field(
+			name = "`//help [command]`",
+			value = "Shows this message. If a command is specified, it gives a more in-depth explanantion.",
+			inline = False
+		)
+		
+		embed.add_field(
+			name = "`//kick <user> [reason]`",
+			value = "Kicks the given user (formatted as a mention).",
+			inline = False
+		)
+	
+		embed.add_field(
+			name = "`//ban <user> [reason]`",
+			value = "Bans the given user (formatted as a mention).",
+			inline = False
+		)
+	
+		embed.add_field(
+			name = "`//unban <user>`",
+			value = "Unbans the given user (formatted as the username with the tag).",
+			inline = False
+		)
+
+	else:
+		embed = discord.Embed(
+			title = "__**Parthird Help**__",
+			colour = discord.Colour.from_rgb(0,150,90)
+		)
+
+		if command == "help":
+			embed.add_field(
+				name = "`//help [command]`",
+				value = "Shows a list of all commands. If a command is specified, then a more detailed message about the command will show.\n\nExample: *//help kick*"
+			)
+		
+		elif command == "kick":
+			embed.add_field(
+				name = "`//kick <user> [reason]`",
+				value = "Kicks the given user. The `<user>` parameter must be a mention/ping.\n\nExample: *//kick* <@960253793063301120> *example use*"
+			)
+
+		elif command == "ban":
+			embed.add_field(
+				name = "`//ban <user> [reason]`",
+				value = "Bans the given user. The `<user>` parameter must be a mention/ping.\n\nExample: *//ban* <@960253793063301120> *example use*"
+			)
+
+		elif command == "unban":
+			embed.add_field(
+				name = "`//unban <user>`",
+				value = "Unbans the given user. The `<user>` parameter must be the username followed by the tag.\n\nExample: *//unban TheFakeKryllyx#0000*"
+			)
+
+	embed.set_footer(text="<> = Required; [] = Optional")
+
+	await ctx.send(embed=embed)
 
 # Kicks the given user.
 @client.command()
