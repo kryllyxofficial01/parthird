78ac79f0f74ebacfe0511494c87d96170957b452
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-08-09 01:07:40 +0000

    stats command (im so tired)

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
+from typing import Union
 import discord
 from discord.ext import commands
 
 # Creates the bot.
-client = commands.Bot(command_prefix="//")
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="//", intents=intents)
 client.remove_command("help")
 
 # Gets the bot's token.
@@ -41,21 +43,29 @@ async def help(ctx, command=None):
 		
 		embed.add_field(
 			name = "`//kick <user> [reason]`",
-			value = "Kicks the given user (formatted as a mention).",
+			value = "Kicks the given user.",
 			inline = False
 		)
 	
 		embed.add_field(
 			name = "`//ban <user> [reason]`",
-			value = "Bans the given user (formatted as a mention).",
+			value = "Bans the given user.",
 			inline = False
 		)
 	
 		embed.add_field(
 			name = "`//unban <user>`",
-			value = "Unbans the given user (formatted as the username with the tag).",
+			value = "Unbans the given user.",
 			inline = False
 		)
+
+		
+		embed.add_field(
+			name = "`//stats <user>`",
+			value = "Gets the technical details of a user.",
+			inline = False
+		)
+
 
 	else:
 		embed = discord.Embed(
@@ -72,19 +82,25 @@ async def help(ctx, command=None):
 		elif command == "kick":
 			embed.add_field(
 				name = "`//kick <user> [reason]`",
-				value = "Kicks the given user. The `<user>` parameter must be a mention/ping.\n\nExample: *//kick* <@960253793063301120> *example use*"
+				value = "Kicks the given user. The `<user>` parameter must be a mention or a user ID.\n\nExample: *//kick* <@960253793063301120> *example use*"
 			)
 
 		elif command == "ban":
 			embed.add_field(
 				name = "`//ban <user> [reason]`",
-				value = "Bans the given user. The `<user>` parameter must be a mention/ping.\n\nExample: *//ban* <@960253793063301120> *example use*"
+				value = "Bans the given user. The `<user>` parameter must be a mention or a user ID.\n\nExample: *//ban* <@960253793063301120> *example use*"
 			)
 
 		elif command == "unban":
 			embed.add_field(
 				name = "`//unban <user>`",
 				value = "Unbans the given user. The `<user>` parameter must be the username followed by the tag.\n\nExample: *//unban TheFakeKryllyx#0000*"
+			)
+
+		elif command == "stats":
+			embed.add_field(
+				name = "`//stats <user>`",
+				value = "Gets the technical details of the given user.\n\n__Details:__\n- Username\n- Tag\n- User ID\n- Nickname\n- Status\n- Roles\n- Date Joined\n\nThe `<user>` parameter must be a mention or a user ID.\n\nExample: *//stats* <@960253793063301120>"
 			)
 
 	embed.set_footer(text="<> = Required; [] = Optional")
@@ -94,7 +110,10 @@ async def help(ctx, command=None):
 # Kicks the given user.
 @client.command()
 @commands.has_permissions(kick_members=True)
-async def kick(ctx, user: discord.Member, *, reason=""):
+async def kick(ctx, user: Union[discord.Member, int], *, reason=""):
+	if type(user) == "<class 'int'>":
+		user = client.get_user(user)
+	
 	if reason:
 		await user.kick(reason=reason)
 		await ctx.send(f"User \"{user}\" has been kicked.\nReason: {reason}")
@@ -114,7 +133,10 @@ async def kick_error(ctx, error):
 # Bans the given user.
 @client.command()
 @commands.has_permissions(ban_members=True)
-async def ban(ctx, user: discord.Member, *, reason=""):
+async def ban(ctx, user: Union[discord.Member, int], *, reason=""):
+	if type(user) == "<class 'int'>":
+		user = client.get_user(user)
+	
 	if reason:
 		await user.ban(reason=reason)
 		await ctx.send(f"User \"{user}\" has been banned.\nReason: {reason}")
@@ -171,7 +193,68 @@ async def unban_error(ctx, error):
 		await ctx.send("The user either does not exist or has not been banned.")
 		return
 
-	print(error)
+@client.command()
+async def stats(ctx, user: Union[discord.Member, int]):
+	if type(user) == "<class 'int'>":
+		user = client.get_user(user)
+
+	embed = discord.Embed(
+		title = "__**User Stats**__",
+		description = "*All of the technical information of a given player.*\n------------------------------------------------------------",
+		colour = discord.Colour.from_rgb(0,150,90)
+	)
+
+	roles = ""
+	for role in user.roles:
+		roles += role.name + ", "
+
+	embed.add_field(
+		name = "Username:",
+		value = user.name,
+		inline = False
+	)
+
+	embed.add_field(
+		name = "Tag:",
+		value = user.discriminator,
+		inline = False
+	)
+
+	embed.add_field(
+		name = "User ID:",
+		value = user.id,
+		inline = False
+	)
+
+	embed.add_field(
+		name = "Nickname:",
+		value = user.nick if user.nick != None else "No nickname",
+		inline = False
+	)
+
+	embed.add_field(
+		name = "Status:",
+		value = user.status,
+		inline = False
+	)
+
+	embed.add_field(
+		name = "Roles:",
+		value = roles.strip(", "),
+		inline = False
+	)
+
+	time = user.joined_at.strftime("%H:%M:%S %p on %A, %B %d, %Y")
+	embed.set_footer(text=f"Joined: {time}")
+
+	await ctx.send(embed=embed)
+	return
+
+@stats.error
+async def stats_error(ctx, error):
+	if isinstance(error, commands.MemberNotFound):
+		await ctx.send("The user either does not exist or has not been formatted correctly. See `//help stats` for more info.")
+		return
 
 # Starts the bot.
 client.run(TOKEN)
